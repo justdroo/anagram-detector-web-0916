b82941ef950f4c9fb5252c66c9d891ec703e100c
Flatiron School <flatironschool@Drew-2.local>
2016-09-29 14:44:41 -0400

    Complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  def initialize(original_word)
+    @anagram_key = original_word.chars.sort
+  end
+
+  def match(word_list)
+    word_list.each_with_object([]) do |word, matches|
+      matches << word if word.chars.sort == @anagram_key
+    end
+  end
+end
